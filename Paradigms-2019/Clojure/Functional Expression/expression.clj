4ac5bcf4291318d38a4ad84f1b47791ee2f010d9
Alexander Varfolomeev <47699145+alexanderlvarfolomeev@users.noreply.github.com>
2019-04-15 23:45:24 +0300

    Clojure homework added

---
+++ Paradigms-2019/Clojure/Functional Expression/expression.clj
@@ -0,0 +1,18 @@
+(defn variable [var] #(% var))
+(defn constant [value] (constantly value))
+
+(defn negate [argument] #(- (argument %)))
+
+(defn operationWithMultipleArguments [f arguments] (fn [coll] (apply f (map #(% coll) arguments))))
+(defn add [& arguments] (operationWithMultipleArguments + arguments))
+(defn subtract [& arguments] (operationWithMultipleArguments - arguments))
+(defn multiply [& arguments] (operationWithMultipleArguments * arguments))
+(defn divide [x y] #(/ (double (x %)) (y %)))               ;;cause java.lang.Number checks division by zero
+
+(def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate})
+
+(defn parseExpression [expr] (cond
+                               (number? expr) (constant expr)
+                               (symbol? expr) (variable (str expr))
+                               :else (apply (operations (first expr)) (map #(parseExpression %) (rest expr)))))
+(defn parseFunction [string] (parseExpression (read-string string)))
