de8c17150927e8134a378564c7dad496b49be3ef
Alexander Varfolomeev <47699145+alexanderlvarfolomeev@users.noreply.github.com>
2019-04-16 00:14:22 +0300

    Update expression.clj

--- Paradigms-2019/Clojure/FunctionalExpression/expression.clj
+++ Paradigms-2019/Clojure/FunctionalExpression/expression.clj
@@ -1,13 +1,14 @@
 (defn variable [var] #(% var))
 (defn constant [value] (constantly value))
 
-(defn negate [argument] #(- (argument %)))
+(defn negate [argument] (comp - argument))
 
 (defn operationWithMultipleArguments [f arguments] (fn [coll] (apply f
                                                                      (map #(% coll) arguments))))
 (defn add [& arguments] (operationWithMultipleArguments + arguments))
 (defn subtract [& arguments] (operationWithMultipleArguments - arguments))
 (defn multiply [& arguments] (operationWithMultipleArguments * arguments))
+
 (defn divide [x y] #(/ (double (x %)) (y %)))               ;;cause java.lang.Number checks division by zero
 
 (def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate})
@@ -15,5 +16,5 @@
 (defn parseExpression [expr] (cond
                                (number? expr) (constant expr)
                                (symbol? expr) (variable (str expr))
-                               :else (apply (operations (first expr)) (map #(parseExpression %) (rest expr)))))
+                               :else (apply (operations (first expr)) (map parseExpression (rest expr)))))
 (def parseFunction (comp parseExpression read-string))
