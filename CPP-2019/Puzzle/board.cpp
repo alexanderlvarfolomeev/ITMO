05f663ea9e16dc9048bc0ba90b9d02cee325a81b
Alexander Varfolomeev <47699145+alexanderlvarfolomeev@users.noreply.github.com>
2019-04-20 23:29:17 +0300

    Update board.cpp

--- CPP-2019/Puzzle/board.cpp
+++ CPP-2019/Puzzle/board.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include <string>
 #include <cmath>
 #include <numeric>
 #include <ctime>
+#include <set>
+#include <map>
 
 size_t random(unsigned int n) {
     return std::time(nullptr) % (n + 1);
@@ -65,15 +68,23 @@ public:
         }
     }
 
-    bool is_space(size_t i, size_t j) {
+    const std::pair<size_t, size_t> get_space() const {
+        return space;
+    }
+
+    std::vector<std::vector<int>> get_state() const {
+        return state;
+    }
+
+    bool is_space(size_t i, size_t j) const {
         return space == std::make_pair(i, j);
     }
 
-    std::pair<unsigned int, unsigned int> size() {
+    std::pair<unsigned int, unsigned int> size() const {
         return {height, width};
     }
 
-    unsigned int hamming() {
+    unsigned int hamming() const {
         unsigned int result = 0;
         for (size_t i = 0; i < height; ++i) {
             for (size_t j = 0; j < width; ++j) {
@@ -85,7 +96,7 @@ public:
         return result;
     }
 
-    unsigned int manhattan() {
+    unsigned int manhattan() const {
         unsigned int result = 0;
         for (auto i = 0; i < height; ++i) {
             for (auto j = 0; j < width; ++j) {
@@ -101,11 +112,11 @@ public:
         return result;
     }
 
-    bool is_goal() {
+    bool is_goal() const {
         return hamming() == 0;
     }
 
-    bool is_solvable() {
+    bool is_solvable() const {
         if (height == 0 || width == 0) {
             return true;
         }
@@ -139,7 +150,7 @@ public:
         return (result + height) % 2 == 0;
     }
 
-    bool operator==(const board &other) {
+    bool operator==(const board &other) const {
         if (width != other.width || height != other.height) {
             return false;
         }
@@ -152,20 +163,41 @@ public:
         return result;
     }
 
-    bool operator!=(const board &other) {
+    bool operator!=(const board &other) const {
         return !operator==(other);
+    }
+
+    bool operator<(const board &other) const {
+        if (height != other.height || width != other.width) {
+            return false;
+        }
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (state[i][j] > other.state[i][j]) {
+                    return false;
+                }
+                if (state[i][j] < other.state[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool operator>(const board &other) const {
+        return other < *this;
     }
 
     std::vector<int> &operator[](int n) {
         return state[n];
     }
 
-    std::string &n_times(std::string &str, const size_t count, const char c = ' ') {
+    static std::string &n_times(std::string &str, const size_t count, const char c = ' ') {
         str.insert(0, count, c);
         return str;
     }
 
-    std::string to_string() {
+    std::string to_string() const {
         std::string result;
         unsigned int number_size = std::to_string(height * width).length();
         std::string empty;
@@ -183,17 +215,106 @@ public:
         return result;
     }
 
-    friend std::ostream &operator<<(std::ostream &stream, board &b) {
+    friend std::ostream &operator<<(std::ostream &stream, const board &b) {
         return stream << b.to_string();
     }
 };
 
+class solver {
+    bool solvable;
+
+    void construct_path(std::map<board, board> &came_from, const board &goal) {
+        std::map<board, board>::iterator current;
+        path.push_front(goal);
+        current = came_from.find(goal);
+        while (current != came_from.end()) {
+            path.push_front(current->second);
+            current = came_from.find(current->second);
+        }
+    }
+
+    void generate_neighbors(const board &b, std::vector<board> &neighbors) {
+        auto[i, j] = b.get_space();
+        auto table = b.get_state();
+        neighbors.clear();
+        if (j != 0) {
+            std::swap(table[i][j], table[i][j - 1]);
+            neighbors.emplace_back(table);
+            std::swap(table[i][j], table[i][j - 1]);
+        }
+        if (j != b.size().second - 1) {
+            std::swap(table[i][j], table[i][j + 1]);
+            neighbors.emplace_back(table);
+            std::swap(table[i][j], table[i][j + 1]);
+        }
+        if (i != 0) {
+            std::swap(table[i][j], table[i - 1][j]);
+            neighbors.emplace_back(table);
+            std::swap(table[i][j], table[i - 1][j]);
+        }
+        if (i != b.size().first - 1) {
+            std::swap(table[i][j], table[i + 1][j]);
+            neighbors.emplace_back(table);
+            std::swap(table[i][j], table[i + 1][j]);
+        }
+    }
+
+public:
+    std::list<board> path;
+
+    explicit solver(const board &b) {
+        solvable = b.is_solvable();
+        if (!solvable) {
+            path = {};
+            return;
+        }
+        std::set<board> closed, opened;
+        opened.insert(b);
+        std::map<board, board> came_from;
+        std::map<board, unsigned int> g_score;
+        std::map<board, unsigned int> f_score;
+        f_score.insert({b, b.manhattan()});
+        g_score.insert({b, 0});
+        std::vector<board> neighbors;
+        while (!opened.empty()) {
+            auto current = *opened.begin();
+            if (current.is_goal()) {
+                construct_path(came_from, current);
+            }
+            opened.erase(current);
+            closed.insert(current);
+            generate_neighbors(current, neighbors);
+            for (int i = 0; i < neighbors.size(); ++i) {
+                if (closed.find(neighbors[i]) != closed.end()) {
+                    continue;
+                }
+                int score = g_score.find(current)->second + 1;
+                if (opened.find(neighbors[i]) == opened.end()) {
+                    opened.insert(neighbors[i]);
+                } else if (score >= g_score.find(neighbors[i])->second) {
+                    continue;
+                }
+                came_from.insert({neighbors[i], current});
+                g_score.insert({neighbors[i], score});
+                f_score.insert({neighbors[i], score + neighbors[i].manhattan()});;
+            }
+        }
+    }
+};
+
 int main() {
-    board b1 = board({{1, 3, 4},
-                      {2, 5, 6}});
-    board b(2, 3);
+    board b1 = board({{8, 3, 4},
+                     {2, 1, 5},
+                     {6, 9, 7}});
+    board b2 = board({{1, 3, 4},
+                      {6, 2, 5}});
+    board b(1, 4);
+    solver s = solver(b);
     std::cout << b << '\n' << '(' << b.size().first << ':' << b.size().second << ')' << ' '
               << b.hamming() << ' ' << b.manhattan() << ' '
-              << (b.is_solvable() ? "solvable" : "not solvable") << '\n';
+              << (b.is_solvable() ? "solvable" : "not solvable") << "\n\n";
+    for (const auto &i : s.path) {
+        std::cout << i << '\n';
+    }
     return 0;
 }
