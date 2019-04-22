0df5a21c59fefa13e9666bf1eb60c8bf35ad89ab
Alexander Varfolomeev <47699145+alexanderlvarfolomeev@users.noreply.github.com>
2019-04-22 23:33:35 +0300

    Update expression.clj

--- Paradigms-2019/Clojure/FunctionalExpression/expression.clj
+++ Paradigms-2019/Clojure/FunctionalExpression/expression.clj
@@ -1,20 +1,33 @@
+;(use '[clojure.math.numeric-tower :as math])
 (defn variable [var] #(% var))
 (defn constant [value] (constantly value))
-
-(defn unaryOperation [f argument] (comp f argument))		;; Muda
+(defn unaryOperation [& functions] (apply comp functions))
 (defn negate [argument] (unaryOperation - argument))
+(defn square [argument] (unaryOperation #(* % %) argument))
+;(defn sqrt [argument] (unaryOperation math/sqrt math/abs argument))
+(defn sqrt [argument] (unaryOperation #(Math/sqrt %) #(Math/abs %) argument))
 (defn sinh [argument] (unaryOperation #(Math/sinh %) argument))
 (defn cosh [argument] (unaryOperation #(Math/cosh %) argument))
 
-(defn operationWithMultipleArguments [f arguments] (fn [coll] (apply f
-                                                                     ((apply juxt arguments) coll))))
+(defn operationWithMultipleArguments
+  [f arguments] (fn [coll] (apply f
+                                  ((apply juxt arguments) coll))))
 (defn add [& arguments] (operationWithMultipleArguments + arguments))
 (defn subtract [& arguments] (operationWithMultipleArguments - arguments))
 (defn multiply [& arguments] (operationWithMultipleArguments * arguments))
 
-(defn divide [x y] #(/ (double (x %)) (y %)))               ;; Cause java.lang.Number checks division by zero
+(def originalMin min)
+(def originalMax max)
+(defn functionalChecker [f arguments] (if (number? (first arguments))
+                                          (apply f arguments)
+                                          (operationWithMultipleArguments f arguments)))
+(defn min [& arguments] (functionalChecker originalMin arguments))
+(defn max [& arguments] (functionalChecker originalMax arguments))
 
-(def operations {'+ add, '- subtract, '* multiply, '/ divide, 'negate negate, 'sinh sinh, 'cosh cosh})
+(defn divide [x y] #(/ (double (x %)) (y %)))               ;;Cause java.lang.Number checks division by zero
+
+(def operations {'+    add, '- subtract, '* multiply, '/ divide, 'negate negate,
+                 'sinh sinh, 'cosh cosh, 'min min, 'max max, 'square square, 'sqrt sqrt})
 
 (defn parseExpression [expr] (cond
                                (number? expr) (constant expr)
